189f1498628fe754f640f25da41943cf054fa487
Dushali-2003 <dushali@aerinit.com>
2025-08-06 16:45:33 +0530

    m

--- variables.tf
+++ variables.tf
@@ -1,3 +1,34 @@
 variable "subnet-1_cidr_block" {
 default = "10.50.0.0/24"
 }
+variable "region" {
+default = "us-east-2"
+}
+variable "profile" {
+default = "student.50"
+}
+variable "vpc_cidr_block" {
+default = "10.50.0.0/16"
+}
+variable "vpc_name" {
+default = "student.50-vpc"
+}
+variable "subnet-1_name" {
+default = "student.50-subnet-1"
+}
+variable "igw_name" {
+default = "student.50-igw"
+}
+variable "route_table_name" {
+default = "student.50-route-table"
+}
+variable "security_group_name" {
+default = "student.50-security-group"
+}
+variable "key_name" {
+default = "student.50-vm-key"
+}
+
+
+
+
