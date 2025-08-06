cbcfcb4649729f98428ba2b0ebe3e6040c99ffad
Dushali-2003 <dushali@aerinit.com>
2025-08-06 14:22:27 +0530

    add route-table-association

--- output.tf
+++ output.tf
@@ -4,3 +4,14 @@ value = aws_vpc.main-vpc.id
 output "my_subnet" {
 value = aws_subnet.subnet-1.id
 }
+output "my_igw" {
+value = aws_internet_gateway.igw.id
+}
+output "my_routetable" {
+value = aws_route_table.my-route-table.id
+}
+output "my_routetableassociation" {
+value = aws_route_table_association.my-route-association.id
+}
+
+
