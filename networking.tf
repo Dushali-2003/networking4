a3e7a343b6157e09481553713cae967c19208699
Dushali-2003 <dushali@aerinit.com>
2025-08-06 14:12:26 +0530

    VPC and subnets

--- networking.tf
+++ networking.tf
@@ -5,3 +5,11 @@ tags = {
 Name = "student.50-vpc"
 }
 }
+resource "aws_subnet" "subnet-1" {
+vpc_id = aws_vpc.main-vpc.id
+cidr_block = "10.50.0.0/24"
+map_public_ip_on_launch = "true"
+tags = {
+Name = "student.50-subnet-1"
+}
+}
--- output.tf
+++ output.tf
@@ -1,3 +1,6 @@
 output "my_vpc" {
 value = aws_vpc.main-vpc.id
 }
+output "my_subnet" {
+value = aws_subnet.subnet-1.id
+}
